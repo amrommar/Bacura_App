3a99afd7764e0c163c2fa9d906258f4253e33305
Amr <amrkhatab1999@gmail.com>
2024-10-27 17:55:04 +0300

    add OpenStreetMap

--- lib/presentation/homeScreen/home_tab/service_request/widgets/requestsent_bottomsheet.dart
+++ lib/presentation/homeScreen/home_tab/service_request/widgets/requestsent_bottomsheet.dart
@@ -3,7 +3,6 @@ import 'package:bacura_app/presentation/resources/color_manager.dart';
 import 'package:bacura_app/presentation/resources/routes_manager.dart';
 import 'package:bacura_app/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class RequestSent_BottomSheet extends StatefulWidget {
   @override
@@ -38,58 +37,6 @@ class _RequestSent_BottomSheetState extends State<RequestSent_BottomSheet> {
                   Navigator.pushNamed(context, Routes.paymentScreenRoute);
                 }),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class CardCustom_TxtField extends StatelessWidget {
-  String hintText;
-  bool isObsucre;
-  var keyboardType;
-  String? Function(String?)? validator;
-  TextEditingController controller;
-  List<TextInputFormatter>? inputFormatter;
-
-  CardCustom_TxtField(
-      {required this.hintText,
-      this.isObsucre = false,
-      this.validator,
-      required this.controller,
-      this.keyboardType = TextInputType.text,
-      this.inputFormatter});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8.0, right: 8, left: 8),
-      child: TextFormField(
-        controller: controller,
-        validator: validator,
-        keyboardType: keyboardType,
-        obscureText: isObsucre,
-        inputFormatters: inputFormatter,
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: ColorManager.soLightGreyColor,
-          // Background color of the TextFormField
-          hintText: hintText,
-          hintStyle: Theme.of(context).textTheme.titleMedium!.copyWith(color: ColorManager.greyColor),
-          contentPadding: EdgeInsets.symmetric(vertical: AppPadding.p4, horizontal: AppPadding.p12),
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0), // Rounded corners
-              borderSide: BorderSide(color: Colors.white) // Border color
-              ),
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0), borderSide: BorderSide(color: ColorManager.whiteColor)),
-          // Border color when not focused
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            borderSide: BorderSide(
-              color: ColorManager.whiteColor, // Border color when focused
-            ),
-          ),
         ),
       ),
     );
