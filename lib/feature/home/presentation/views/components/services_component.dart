84d7776ecc84805e3fbfc812d6f4794b4aa16efd
Abdalrahman Bakr <abdalrahmanbakr.dev@gmail.com>
2025-01-26 17:31:15 +0200

    ..

--- lib/feature/home/presentation/views/components/services_component.dart
+++ lib/feature/home/presentation/views/components/services_component.dart
@@ -1,5 +1,6 @@
 import 'package:bacura_app/core/utils/index.dart';
 import 'package:bacura_app/feature/home/index.dart';
+import 'package:bacura_app/feature/home/presentation/controller/home_provider.dart';
 
 class ServicesComponent extends StatelessWidget {
   ServicesComponent({super.key});
@@ -40,42 +41,44 @@ class ServicesComponent extends StatelessWidget {
       AppStrings.computerMaintenance,
       AppStrings.phoneMaintenance
     ];
-    return Container(
-        padding: EdgeInsets.all(AppSizes.ph4),
-        decoration: BoxDecoration(
-          color: ColorManager.midWhiteColor,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-                padding: EdgeInsets.only(right: AppSizes.pw16, left: AppSizes.pw16, top: AppSizes.ph6),
-                child: Text(AppLocalizations.of(context)!.services,
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          color: ColorManager.blackColor,
-                          fontWeight: FontWeight.bold,
-                        ))),
-            GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 6,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 0,
-                  mainAxisSpacing: 5,
-                  childAspectRatio: 0.89,
-                ),
-                itemBuilder: (context, index) {
-                  return InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, Routes.serviceTypeRoute);
-                      },
-                      child: ServiceCardWidget(
-                        departmentTitle: imagesTitle[index],
-                        imagePath: imagesPaths[index],
-                      ));
-                }),
-          ],
-        ));
+    return Consumer<HomeProvider>(
+      builder: (context, provider, child) => Container(
+          padding: EdgeInsets.all(AppSizes.ph4),
+          decoration: BoxDecoration(
+            color: ColorManager.midWhiteColor,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(right: AppSizes.pw16, left: AppSizes.pw16, top: AppSizes.ph6),
+                  child: Text(AppLocalizations.of(context)!.services,
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            color: ColorManager.blackColor,
+                            fontWeight: FontWeight.bold,
+                          ))),
+              GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: provider.categoryEntity.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 0,
+                    mainAxisSpacing: 5,
+                    childAspectRatio: 0.89,
+                  ),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, Routes.serviceTypeRoute, arguments: provider.categoryEntity[index].id);
+                        },
+                        child: ServiceCardWidget(
+                          departmentTitle: provider.categoryEntity[index].name!,
+                          imagePath: provider.categoryEntity[index].image!,
+                        ));
+                  }),
+            ],
+          )),
+    );
   }
 }
