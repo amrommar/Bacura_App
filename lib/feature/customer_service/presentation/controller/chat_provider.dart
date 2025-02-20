1fa88b7397ef6a6f17a172285938795f89171a0f
Abdalrahman Bakr <abdalrahmanbakr.dev@gmail.com>
2025-02-21 01:08:01 +0200

    provider chat

--- lib/feature/customer_service/presentation/controller/chat_provider.dart
+++ lib/feature/customer_service/presentation/controller/chat_provider.dart
@@ -1,3 +1,47 @@
 import 'package:bacura_app/core/utils/index.dart';
+import 'package:bacura_app/feature/customer_service/domain/entity/chat_entity.dart';
+import 'package:bacura_app/feature/customer_service/domain/entity/my_message_chat_entity.dart';
+import 'package:bacura_app/feature/customer_service/domain/use_case/get_message_chat_use_case.dart';
+import 'package:bacura_app/feature/customer_service/domain/use_case/get_my_chat_use_case.dart';
+import 'package:bacura_app/feature/customer_service/domain/use_case/send_message_use_case.dart';
 
-class ChatProvider with ChangeNotifier {}
+class ChatProvider with ChangeNotifier {
+  List<MyMessageChatEntity> myMessageChatEntity = [];
+  ChatEntity? getMessagesChat;
+  bool isLoading = true;
+  String content = '';
+
+  ChatProvider() {
+    init();
+  }
+
+  init() async {
+    await _getMine();
+    await _getChatMessages();
+  }
+
+  Future<void> _getMine() async {
+    var result = await sl<GetMyChatUseCase>().call();
+    result.fold((l) async {}, (r) async {
+      getMessagesChat = r;
+      isLoading = false;
+      notifyListeners();
+    });
+  }
+
+  Future<void> _getChatMessages() async {
+    var result = await sl<GetMessageChatUseCase>().call(getMessagesChat!.id);
+    result.fold((l) async {}, (r) async {
+      myMessageChatEntity = r;
+      isLoading = false;
+      notifyListeners();
+    });
+  }
+
+  Future<void> sendMessage() async {
+    var result = await sl<SendMessageUseCase>().call(SendMessageParams(id: getMessagesChat!.id, content: content));
+    result.fold((l) async {}, (r) async {
+      notifyListeners();
+    });
+  }
+}
--- lib/feature/customer_service/presentation/views/customer_service_screen.dart
+++ lib/feature/customer_service/presentation/views/customer_service_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bacura_app/core/utils/index.dart';
 import 'package:bacura_app/feature/customer_service/index.dart';
+import 'package:bacura_app/feature/customer_service/presentation/controller/chat_provider.dart';
 
 class CustomerServiceScreen extends StatelessWidget {
   const CustomerServiceScreen({super.key});
@@ -14,38 +15,43 @@ class CustomerServiceScreen extends StatelessWidget {
             actions: const [
               CustomerServiceAppBarWidget(),
             ]),
-        body: Column(children: [
-          Expanded(
-              child: ListView(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: AppSizes.pw16,
-                    vertical: AppSizes.ph24,
-                  ),
-                  children: [
-                CustomerServiceMessageWidget(
-                  sender: "خدمة العملاء",
-                  message: "مرحبًا بك في خدمة الدردشة للعملاء.",
-                  time: "11:25 ص",
-                ),
-                CustomerServiceMessageWidget(
-                  sender: "خدمة العملاء",
-                  message: "كيف يمكننا مساعدتك اليوم؟",
-                  time: "11:26 ص",
-                ),
-                UserMessageWidget(message: "المدفوعات", time: "11:27 ص"),
-                CustomerServiceMessageWidget(
-                  sender: "خدمة العملاء",
-                  message: "حدد الموضوع الذي تحتاج إلى مساعدة فيه.",
-                  time: "11:29 ص",
-                ),
-                UserMessageWidget(message: "عملية دفع", time: "11:30 ص"),
-                CustomerServiceMessageWidget(
-                  sender: "خدمة العملاء",
-                  message: "حدد طريقة الدفع التي تحتاج إلى مساعدة بشأنها",
-                  time: "11:32 am",
-                ),
-              ])),
-          const WriteMessageWidget()
-        ]));
+        body: ChangeNotifierProvider<ChatProvider>(
+          create: (context) => ChatProvider(),
+          child: Consumer<ChatProvider>(
+            builder: (context, provider, child) => Column(children: [
+              Expanded(
+                  child: ListView(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: AppSizes.pw16,
+                        vertical: AppSizes.ph24,
+                      ),
+                      children: [
+                    CustomerServiceMessageWidget(
+                      sender: "خدمة العملاء",
+                      message: "مرحبًا بك في خدمة الدردشة للعملاء.",
+                      time: "11:25 ص",
+                    ),
+                    CustomerServiceMessageWidget(
+                      sender: "خدمة العملاء",
+                      message: "كيف يمكننا مساعدتك اليوم؟",
+                      time: "11:26 ص",
+                    ),
+                    UserMessageWidget(message: "المدفوعات", time: "11:27 ص"),
+                    CustomerServiceMessageWidget(
+                      sender: "خدمة العملاء",
+                      message: "حدد الموضوع الذي تحتاج إلى مساعدة فيه.",
+                      time: "11:29 ص",
+                    ),
+                    UserMessageWidget(message: "عملية دفع", time: "11:30 ص"),
+                    CustomerServiceMessageWidget(
+                      sender: "خدمة العملاء",
+                      message: "حدد طريقة الدفع التي تحتاج إلى مساعدة بشأنها",
+                      time: "11:32 am",
+                    ),
+                  ])),
+              const WriteMessageWidget()
+            ]),
+          ),
+        ));
   }
 }
