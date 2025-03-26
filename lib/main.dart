e34a3aa6d1707a8c53bb1f35cac1416099efd259
Abdalrahman Bakr <abdalrahmanbakr.dev@gmail.com>
2025-03-26 23:05:58 +0200

    Connectivity

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:bacura_app/core/utils/index.dart';
 import 'package:bacura_app/feature/home/presentation/controller/home_provider.dart';
 import 'package:bacura_app/feature/order_services/presentation/controller/order_services_provider.dart';
 import 'package:bacura_app/feature/profile/presentation/controller/my_profile_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await ServiceInitializer().initializeSettings();
@@ -32,6 +36,66 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _checkInternetConnection();
+  }
+
+  void _checkInternetConnection() {
+    InternetConnectionChecker.createInstance().onStatusChange.listen((status) {
+      if (status == InternetConnectionStatus.disconnected) {
+        _showNoInternetOverlay();
+      } else {
+        _removeNoInternetOverlay();
+      }
+    });
+  }
+
+  OverlayEntry? _overlayEntry;
+
+  void _showNoInternetOverlay() {
+    if (_overlayEntry != null) return;
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: 50,
+        left: 20,
+        right: 20,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.wifi_off, color: Colors.white),
+                SizedBox(width: 10),
+                Text(
+                  "لا يوجد اتصال بالإنترنت",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+    _navigatorKey.currentState?.overlay?.insert(_overlayEntry!);
+  }
+
+  void _removeNoInternetOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<LanguageProvider>(context);
@@ -40,6 +104,7 @@ class _MyAppState extends State<MyApp> {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) => MaterialApp(
+        navigatorKey: _navigatorKey,
         locale: Locale(provider.appLanguage),
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
