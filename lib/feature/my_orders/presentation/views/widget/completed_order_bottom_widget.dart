79f5f5411067f63fd0122b4d5a657f7c337f6f2d
Abdalrahman Bakr <abdalrahmanbakr.dev@gmail.com>
2025-03-26 22:37:51 +0200

    modify invoice

--- lib/feature/my_orders/presentation/views/widget/completed_order_bottom_widget.dart
+++ lib/feature/my_orders/presentation/views/widget/completed_order_bottom_widget.dart
@@ -5,6 +5,8 @@ import 'package:bacura_app/feature/invoice/model/customer.dart';
 import 'package:bacura_app/feature/invoice/model/invoice.dart';
 import 'package:bacura_app/feature/invoice/model/supplier.dart';
 import 'package:bacura_app/feature/invoice/widget/button_widget.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CompletedOrderBottomWidget extends StatelessWidget {
   final List<Map<String, dynamic>> invoiceData;
@@ -23,42 +25,58 @@ class CompletedOrderBottomWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ButtonWidget(
-      text: 'تحميل الفاتورة',
-      onClicked: () async {
-        final date = DateTime.now();
-        final List<InvoiceItem> items = invoiceData.map((item) {
-          return InvoiceItem(
-            description: item['description'],
-            date: item['date'] ?? DateTime.now(),
-            quantity: item['quantity'],
-            vat: item['vat'],
-            unitPrice: item['unitPrice'],
-          );
-        }).toList();
+        text: 'تحميل الفاتورة',
+        onClicked: () async {
+          final status = await Permission.storage.request(); // طلب إذن الوصول للتخزين
+          if (status.isGranted) {
+            final date = DateTime.now();
+            final List<InvoiceItem> items = invoiceData.map((item) {
+              return InvoiceItem(
+                description: item['description'],
+                date: item['date'] ?? DateTime.now(),
+                quantity: item['quantity'],
+                vat: item['vat'],
+                unitPrice: item['unitPrice'],
+              );
+            }).toList();
 
-        final invoice = Invoice(
-          supplier: const Supplier(
-            name: 'Bacura Tec',
-            address: 'Al-Narjis, Anas Bin Malik Street',
-            paymentInfo: 'https://paypal.me/sarahfieldzz',
-          ),
-          customer: Customer(
-            mobileNumber: mobileNumber,
-            name: name,
-            address: address,
-          ),
-          info: InvoiceInfo(
-            date: date,
-            description: 'My description...',
-            number: '${DateTime.now().year}',
-          ),
-          items: items,
-        );
+            final invoice = Invoice(
+              supplier: const Supplier(
+                name: 'Bacura Tec',
+                address: 'Al-Narjis, Anas Bin Malik Street',
+                paymentInfo: 'https://paypal.me/sarahfieldzz',
+              ),
+              customer: Customer(
+                mobileNumber: mobileNumber,
+                name: name,
+                address: address,
+              ),
+              info: InvoiceInfo(
+                date: date,
+                description: 'My description...',
+                number: '${DateTime.now().year}',
+              ),
+              items: items,
+            );
 
-        final pdfFile = await PdfInvoiceApi.generate(invoice);
+            final pdfFile = await PdfInvoiceApi.generate(invoice);
 
-        PdfApi.openFile(pdfFile);
-      },
-    );
+            // حفظ الفاتورة في مجلد التنزيلات (Downloads)
+            final directory = Directory('/storage/emulated/0/Download'); // مجلد التنزيلات
+            final filePath = '${directory.path}/Invoice_${DateTime.now().millisecondsSinceEpoch}.pdf';
+            final newFile = File(filePath);
+            await newFile.writeAsBytes(await pdfFile.readAsBytes());
+
+            // إظهار إشعار للمستخدم بمكان الملف
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('تم تحميل الفاتورة بنجاح: $filePath')),
+            );
+          } else {
+            // لو المستخدم رفض الإذن
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('يجب السماح بالوصول إلى التخزين لحفظ الفاتورة')),
+            );
+          }
+        });
   }
 }
